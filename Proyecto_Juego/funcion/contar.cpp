3f3e682c9f56fb3133fb475554c8ea275decf457
Ricardo Alexei Valle <00080525@uca.edu.sv>
2025-06-02 22:53:58 -0600

    Declarar funcion y llamar función en otra zona
    Concepto básico de como llama a una función para que la llamen y tengamos un código más ordenado.

---
+++ Proyecto_Juego/funcion/contar.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void contar() {
+    int n;
+    do {
+        cout << "Da numeros que no sean 0" << endl;
+        cin >> n;
+    } while (n != 0);
+}
---
+++ Proyecto_Juego/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "funcion/contar.cpp"
+using namespace std;
+
+int main(){
+
+    cout << "Hola!!" << endl;
+    cout << "Vamos a contar sin decir 0!" << endl;
+    contar();
+
+    return 0;
+}
